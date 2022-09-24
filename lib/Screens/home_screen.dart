e326fc1d9e65cbca105560c3c1ac94ee7c93cd61
Wise-Wizard <saransh.shankar@gmail.com>
2022-09-24 12:14:48 +0530

    Casfree API Integration

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:cashfree_pg/cashfree_pg.dart';
+import 'package:flutter_application_7/transaction.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,13 +15,16 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
+      appBar: AppBar(
+        title: const Text('Cash Manager'),
+        centerTitle: true,
+      ),
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
         Container(
-          color: Colors.white,
+          margin: const EdgeInsets.all(30),
+          child: TextField(),
         ),
-        Container(
-          color: Colors.blue,
-        )
+        ElevatedButton(onPressed: doTransaction, child: Text('Pay'))
       ]),
     );
   }
---
+++ lib/transaction.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: avoid_print
+
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+Future<String> doTransaction() async {
+  var request = await http.post(
+    Uri.https("test.cashfree.com", "api/v2/cftoken/order"),
+    headers: <String, String>{
+      'x-client-id': '2389568c3805543a9f8925f5ff659832',
+      'x-client-secret': '9cb0079daa909113dc41d1481a1230cb102d7112',
+      'Content-Type': 'application/json'
+    },
+    body: jsonEncode(
+        {"orderId": "1111", "orderAmount": "6000", "orderCurrency": "INR"}),
+  );
+  if (request.statusCode == 200) {
+    var jsonResponse = jsonDecode(request.body);
+    if (jsonResponse['status'] == 'OK') {
+      print(jsonResponse['cftoken']);
+      return jsonResponse['cftoken'];
+    }
+  }
+  return 'Invalid cfToken';
+}
