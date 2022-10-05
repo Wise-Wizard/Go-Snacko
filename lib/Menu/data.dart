6c71f465dc4614d7896df7dd5e5e235682bdf162
Wise-Wizard <saransh.shankar@gmail.com>
2022-10-05 15:06:54 +0530

    Added Order History List View

--- lib/Menu/data.dart
+++ lib/Menu/data.dart
@@ -5,53 +5,53 @@ import 'package:flutter_application_7/Models/user.dart';
 
 //Food
 final _burrito =
-    Food(imageUrl: 'assets/images/burrito.jpg', name: 'Burrito', price: 8.99);
+    Food(imageUrl: 'Food_Images/burrito.jpg', name: 'Burrito', price: 8.99);
 final _steak =
-    Food(imageUrl: 'assets/images/steak.jpg', name: 'Steak', price: 17.99);
+    Food(imageUrl: 'Food_Images/steak.jpg', name: 'Steak', price: 17.99);
 final _pasta =
-    Food(imageUrl: 'assets/images/pasta.jpg', name: 'Pasta', price: 14.99);
+    Food(imageUrl: 'Food_Images/pasta.jpg', name: 'Pasta', price: 14.99);
 final _ramen =
-    Food(imageUrl: 'assets/images/ramen.jpg', name: 'Ramen', price: 13.99);
+    Food(imageUrl: 'Food_Images/ramen.jpg', name: 'Ramen', price: 13.99);
 final _pancakes =
-    Food(imageUrl: 'assets/images/pancakes.jpg', name: 'Pancakes', price: 9.99);
+    Food(imageUrl: 'Food_Images/pancakes.jpg', name: 'Pancakes', price: 9.99);
 final _burger =
-    Food(imageUrl: 'assets/images/burger.jpg', name: 'Burger', price: 14.99);
+    Food(imageUrl: 'Food_Images/burger.jpg', name: 'Burger', price: 14.99);
 final _pizza =
-    Food(imageUrl: 'assets/images/pizza.jpg', name: 'Pizza', price: 11.99);
-final _salmon = Food(
-    imageUrl: 'assets/images/salmon.jpg', name: 'Salmon Salad', price: 12.99);
+    Food(imageUrl: 'Food_Images/pizza.jpg', name: 'Pizza', price: 11.99);
+final _salmon =
+    Food(imageUrl: 'Food_Images/salmon.jpg', name: 'Salmon', price: 12.99);
 
 // Restaurants
 final _restaurant0 = Restaurant(
-  imageUrl: 'assets/images/restaurant0.jpg',
+  imageUrl: 'Food_Images/restaurant0.jpg',
   name: 'Restaurant 0',
   address: '200 Main St, New York, NY',
   rating: 5,
   menu: [_burrito, _steak, _pasta, _ramen, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant1 = Restaurant(
-  imageUrl: 'assets/images/restaurant1.jpg',
+  imageUrl: 'Food_Images/restaurant1.jpg',
   name: 'Restaurant 1',
   address: '200 Main St, New York, NY',
   rating: 4,
   menu: [_steak, _pasta, _ramen, _pancakes, _burger, _pizza],
 );
 final _restaurant2 = Restaurant(
-  imageUrl: 'assets/images/restaurant2.jpg',
+  imageUrl: 'Food_Images/restaurant2.jpg',
   name: 'Restaurant 2',
   address: '200 Main St, New York, NY',
   rating: 4,
   menu: [_steak, _pasta, _pancakes, _burger, _pizza, _salmon],
 );
 final _restaurant3 = Restaurant(
-  imageUrl: 'assets/images/restaurant3.jpg',
+  imageUrl: 'Food_Images/restaurant3.jpg',
   name: 'Restaurant 3',
   address: '200 Main St, New York, NY',
   rating: 2,
   menu: [_burrito, _steak, _burger, _pizza, _salmon],
 );
 final _restaurant4 = Restaurant(
-  imageUrl: 'assets/images/restaurant4.jpg',
+  imageUrl: 'Food_Images/restaurant4.jpg',
   name: 'Restaurant 4',
   address: '200 Main St, New York, NY',
   rating: 3,
--- lib/WIdgets/recent_orders.dart
+++ lib/WIdgets/recent_orders.dart
@@ -2,26 +2,94 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_application_7/Menu/data.dart';
+import 'package:flutter_application_7/Models/order.dart';
 
 class RecentOrders extends StatelessWidget {
   const RecentOrders({Key? key}) : super(key: key);
+  _buildOrderHistory(BuildContext context, Order order) {
+    return Container(
+      margin: EdgeInsets.all(10),
+      width: 320,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        color: Color(0xFF00FFD1),
+        border: Border.all(width: 2, color: Color(0xffFF1E1E)),
+      ),
+      child: Expanded(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(15),
+              child: Image(
+                width: 130,
+                height: 130,
+                image: AssetImage(order.food.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  order.food.name,
+                  style: TextStyle(
+                      fontSize: 25,
+                      color: Colors.black87,
+                      fontWeight: FontWeight.w900),
+                ),
+                Text(
+                  order.restaurant.name,
+                  style: TextStyle(fontSize: 20, color: Colors.black87),
+                ),
+                Text(
+                  order.date,
+                  style: TextStyle(fontSize: 20, color: Colors.black87),
+                ),
+              ],
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            FloatingActionButton(
+              onPressed: () {},
+              backgroundColor: Color(
+                0xffFF1E1E,
+              ),
+              child: Icon(
+                Icons.add,
+                size: 35,
+                color: Color(0xFF31C6D4),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Text(
-          'Recent Orders',
+          'Order History',
           style: TextStyle(color: Colors.black87, fontSize: 35),
         ),
         Container(
           padding: EdgeInsets.all(10),
-          height: 120,
-          color: Colors.black,
+          height: 130,
           child: ListView.builder(
+            physics: BouncingScrollPhysics(),
+            scrollDirection: Axis.horizontal,
             itemCount: currentUser.orders.length,
             itemBuilder: (BuildContext context, int index) {
-              return Container();
+              Order order = currentUser.orders[index];
+              return _buildOrderHistory(context, order);
             },
           ),
         ),
